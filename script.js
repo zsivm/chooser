be0a1a17286a3c5509b6f3dd349147977e202e07
Zsivm <zsivics.mihael@gmail.com>
2020-02-14 23:00:27 +0100

    refactored the theme, it is now generalised, and not game specific anymore

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 let number = 0;
 
-// adds a game input with corresponding delete button
-function addGame() {
+// adds an item input with corresponding delete button
+function addItem() {
     // main container with id
-    let oGameCont = document.createElement("div");
-    oGameCont.id = "cont" + number;
+    let oItemCont = document.createElement("div");
+    oItemCont.id = "cont" + number;
     number++;
 
     // input field
-    let oGameInput = document.createElement("input");
-    oGameInput.placeholder = "Add a game...";
+    let oItemInput = document.createElement("input");
+    oItemInput.placeholder = "Add an item...";
 
     // remove button
     let oRemoveBtn = document.createElement("button");
-    oRemoveBtn.onclick = removeGame;
+    oRemoveBtn.onclick = removeItem;
     oRemoveBtn.innerHTML = "X";
 
     // append field and delete to parent
-    oGameCont.appendChild(oGameInput);
-    oGameCont.appendChild(oRemoveBtn);
+    oItemCont.appendChild(oItemInput);
+    oItemCont.appendChild(oRemoveBtn);
 
     // append parent to outer div
-    let oGames = document.getElementById("games");
-    oGames.appendChild(oGameCont);
+    let oItems = document.getElementById("items");
+    oItems.appendChild(oItemCont);
 }
 
-// removes a game row
-function removeGame(e) {
+// removes an item row
+function removeItem(e) {
     let parentDivId = e.currentTarget.parentNode.id;
     let div = document.getElementById(parentDivId);
     div.parentNode.removeChild(div);
 }
 
-// chooses game and displays it on the screen
-function chooseGame() {
-    let oGames = document.getElementById("games").childNodes;
-    if(oGames.length < 1) console.log("No games added");
+// chooses item and displays it on the screen
+function chooseItem() {
+    let oItems = document.getElementById("items").childNodes;
+    if(oItems.length < 1) console.log("No items added");
     
-    let arr = [];
-    oGames.forEach(node => {
-        arr.push(node.getElementsByTagName("INPUT")[0].value);
+    let aItems = [];
+    oItems.forEach(node => {
+        aItems.push(node.getElementsByTagName("INPUT")[0].value);
     });
 
-    let rndNum = Math.floor(Math.random() * arr.length);
-    let winner = oGames[rndNum].getElementsByTagName("INPUT")[0].value;
+    let nRndNum = Math.floor(Math.random() * aItems.length);
+    let sChoosenItem = oItems[nRndNum].getElementsByTagName("INPUT")[0].value;
 
-    document.getElementById("result").innerHTML = winner;
+    document.getElementById("result").innerHTML = sChoosenItem;
 }
